e25d59a60a4301e55e08e6052999cb7240b000cc
Justin <Buntworthy@users.noreply.github.com>
2016-09-27 16:36:18 +0100

    Shorten the longer of two vectors.

--- +jp/trim.m
+++ +jp/trim.m
@@ -2,7 +2,7 @@ function [out1, out2] = trim(vector1, vector2)
 % TRIM shorten the longer of two vectors.
 
 if ~isvector(vector1) || ~isvector(vector2) 
-    throw(MException('BabyCry:TypeError', 'Expected vector, got array'));
+    throw(MException('MatlabUtils:TypeError', 'Expected vector, got array'));
 end
 
 newLength = min(length(vector1), length(vector2));
